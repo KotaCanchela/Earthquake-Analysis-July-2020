985206b8f9e0a77a8b3e18d1418bf891756eb90b
KotaCanchela <dakotacanchela@gmail.com>
2020-07-31 19:46:06 -0700

    Data filtered and program complete

--- eq_last_30_days.py
+++ eq_last_30_days.py
@@ -4,18 +4,17 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Initial data load.
-filename = ""
+filename = "earthquakedataraw.json"
 with open(filename) as f:
     all_eq_data = json.load(f)
 
 # Making the data more readable
-readable_file = ""
+readable_file = "earthquake_data_filtered.json"
 with open(readable_file, "w") as f:
     json.dump(all_eq_data, f, indent=4)
 
 # Obtaining the total amount of earthquakes occurred in the data
 all_eq_dicts = all_eq_data["features"]  # Dictionary now stored in a list
-print(len(all_eq_dicts))
 
 # Pull the magnitude, longitude, and latitude of each earthquake
 mags, lons, lats, hover_texts = [], [], [], []
@@ -24,9 +23,7 @@ for eq_dict in all_eq_dicts:
     hover_texts.append(eq_dict["properties"]["title"])
     lons.append(eq_dict["geometry"]["coordinates"][0])
     lats.append(eq_dict["geometry"]["coordinates"][1])
-print(mags[:10])
-print(lons[:10])
-print(lats[:10])
+
 
 # Map the earthquakes
 
